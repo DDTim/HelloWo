0707f3a95c2057e7f0b70c485893447f336f083e
DDTim <NIKITA13.102010@mail.ru>
2017-04-01 23:23:52 +0600

    sec

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("!");
-        System.out.println("!");
+        System.out.println("Я родился!");
+
         // write your code here
     }
 }
