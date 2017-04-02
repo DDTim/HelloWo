db50ad0ccfdaad673a886448801c2bc42f009362
DDTim <NIKITA13.102010@mail.ru>
2017-04-03 01:02:12 +0600

    less 8
    Loops

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,9 @@
 package com.company;
-import  java.util.Scanner;
+//import  java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String str = "Hello";
+ /*       String str;
         //System.out.println(str);
         Scanner num = new Scanner(System.in);
         int f, s; //r;
@@ -15,13 +15,28 @@ public class Main {
         System.out.println(f + " - " +s+ " = " +(f-s));
         System.out.println(f + " % " +s+ " = " +(f%s));
 
-        //less 7
+//less 7
 
-        String r = (f % 2) == 0 ? " Четное" : " Не четное";
-        System.out.println(f + r);
-        r = (s % 2) == 0 ? " Четное" : " Не четное";
-        System.out.println(s + r);
-        r = (s!=f && s == 3)? "s!= f и s == 3": "не";
-        System.out.println(r);
+        str = (f % 2) == 0 ? " Четное" : " Не четное";
+        System.out.println(f + str);
+
+        str = (s % 2) == 0 ? " Четное" : " Не четное";
+        System.out.println(s + str);
+
+        str = (s!=f || s == 3)? "s!= f и s == 3": "не";
+        System.out.println(str);
+
+        str = f == 7? "f = 7":(f==3? "f = 3" : "Не то!");
+        System.out.println(str);*/
+        // less 8
+        for (int i = 10; i < 21; i++)
+            System.out.println(i * i);
+        for (int i = 25; i < 30; i++)
+            if (i != 27) System.out.println(i);
+        int y = 25;
+        while (y < 30) {
+            if (y != 27) System.out.println(y);
+            y++;
+        }
     }
 }
