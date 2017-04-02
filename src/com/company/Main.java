71f4707f0b555814b601078dea45b8725f6bef2d
DDTim <NIKITA13.102010@mail.ru>
2017-04-02 14:48:00 +0600

    lesson6
    Mathematical  operations

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,19 @@
 package com.company;
-
+import  java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Я родился!");
+        String str = "Hello";
+        //System.out.println(str);
+        Scanner num = new Scanner(System.in);
+        int f, s,r;
+        System.out.println("Enter f num: ");
+        f = num.nextInt();
+        System.out.println("Enter s num: ");
+        s = num.nextInt();
+        System.out.println(f + " + " +s+ " = " +(f+s));
+        System.out.println(f + " - " +s+ " = " +(f-s));
+        System.out.println(f + " % " +s+ " = " +(f%s));
 
-        // write your code here
     }
 }
