e2904a28ef1415d58cdb1a187e227dd520eef789
DDTim <NIKITA13.102010@mail.ru>
2017-04-02 16:33:17 +0600

    lessson 7
    Conditional execution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
         String str = "Hello";
         //System.out.println(str);
         Scanner num = new Scanner(System.in);
-        int f, s,r;
+        int f, s; //r;
         System.out.println("Enter f num: ");
         f = num.nextInt();
         System.out.println("Enter s num: ");
@@ -15,5 +15,13 @@ public class Main {
         System.out.println(f + " - " +s+ " = " +(f-s));
         System.out.println(f + " % " +s+ " = " +(f%s));
 
+        //less 7
+
+        String r = (f % 2) == 0 ? " Четное" : " Не четное";
+        System.out.println(f + r);
+        r = (s % 2) == 0 ? " Четное" : " Не четное";
+        System.out.println(s + r);
+        r = (s!=f && s == 3)? "s!= f и s == 3": "не";
+        System.out.println(r);
     }
 }
